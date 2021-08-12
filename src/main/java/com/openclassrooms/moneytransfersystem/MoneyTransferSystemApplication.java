525a9763ee44b92d4bc04b54d4fa5cd37c412aa2
David <d6797125@pm.me>
2021-08-12 17:18:49 +0200

    refactor: update class names

--- src/main/java/com/openclassrooms/moneytransfersystem/MoneyTransferSystemApplication.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/MoneyTransferSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoneytransfersystemApplication {
+public class MoneyTransferSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MoneytransfersystemApplication.class, args);
+        SpringApplication.run(MoneyTransferSystemApplication.class, args);
     }
 
 }
