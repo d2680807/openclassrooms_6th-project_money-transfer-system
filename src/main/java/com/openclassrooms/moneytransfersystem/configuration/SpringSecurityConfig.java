767b149c53f0469ff20b0745f1b84379d79b1452
David <d6797125@pm.me>
2021-08-12 17:39:21 +0200

    feat(security): add Filter Chain

--- src/main/java/com/openclassrooms/moneytransfersystem/configuration/SpringSecurityConfig.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/configuration/SpringSecurityConfig.java
@@ -1,13 +1,26 @@
 package com.openclassrooms.moneytransfersystem.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("robertdupont@someHost.com")
+                .password(passwordEncoder().encode("jamesbond"))
+                .roles("CLIENT");
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -17,5 +30,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 }
