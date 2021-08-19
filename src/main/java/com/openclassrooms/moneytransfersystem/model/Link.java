a21682977a349ed46c2c5b2a1be38ca8b04c8c21
David <d6797125@pm.me>
2021-08-19 21:52:46 +0200

    feat(model): add Link

---
+++ src/main/java/com/openclassrooms/moneytransfersystem/model/Link.java
@@ -0,0 +1,22 @@
+package com.openclassrooms.moneytransfersystem.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@Entity
+@Table(name = "link")
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "link_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String bankAccount;
+    @Column(nullable = false)
+    private String swiftCode;
+}
