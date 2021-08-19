3280094ec168dca2b6fe0d8bd5529522cf59749c
David <d6797125@pm.me>
2021-08-19 21:59:39 +0200

    feat(model): add User multiplicity relations

--- src/main/java/com/openclassrooms/moneytransfersystem/model/User.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/model/User.java
@@ -33,4 +33,39 @@ public class User {
     String lastName;
     @Column(nullable = false)
     float balance;
+
+    /*@OneToOne(mappedBy = "user")
+    private UsersDetails usersDetails;*/
+
+    /*@ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_details",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "detail_id")
+    )
+    private Collection<Detail> details;*/
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_links",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "link_id")
+    )
+    private Collection<Link> links;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "friendship",
+            joinColumns = @JoinColumn(name = "invited_by"),
+            inverseJoinColumns = @JoinColumn(name = "recipient")
+    )
+    private Collection<Friendship> friendships;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_transfers",
+            joinColumns = @JoinColumn(name = "made_by"),
+            inverseJoinColumns = @JoinColumn(name = "transfer_id")
+    )
+    private Collection<Transfer> transfers;
 }
