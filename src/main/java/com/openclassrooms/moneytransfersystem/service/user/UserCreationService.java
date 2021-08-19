fb62549967d83b625d4a15eb0fe5501b34374bbd
David <d6797125@pm.me>
2021-08-19 22:06:10 +0200

    feat(service): add UserCreationService

---
+++ src/main/java/com/openclassrooms/moneytransfersystem/service/user/UserCreationService.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.moneytransfersystem.service.user;
+
+import com.openclassrooms.moneytransfersystem.dao.UserRepository;
+import com.openclassrooms.moneytransfersystem.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class UserCreationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    Logger logger = LoggerFactory.getLogger(UserCreationService.class);
+
+    public User createUser(User user) {
+
+        logger.debug("Create single " + User.class.getName() + ": " + user);
+
+        return userRepository.save(user);
+    }
+
+    public Collection<User> createUsers(Collection<User> users) {
+
+        logger.debug("Create multiple " + User.class.getName() + ": " + users);
+
+        return userRepository.saveAll(users);
+    }
+}
