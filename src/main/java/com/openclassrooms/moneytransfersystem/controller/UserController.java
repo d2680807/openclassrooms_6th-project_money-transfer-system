1cb022f9b927de276baed15a89549e03a35e5bd9
David <d6797125@pm.me>
2021-08-23 22:37:17 +0200

    feat(controller): add UserController

--- src/main/java/com/openclassrooms/moneytransfersystem/controller/UserController.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/controller/UserController.java
@@ -2,6 +2,7 @@ package com.openclassrooms.moneytransfersystem.controller;
 
 import com.openclassrooms.moneytransfersystem.model.User;
 import com.openclassrooms.moneytransfersystem.service.user.UserCreationService;
+import com.openclassrooms.moneytransfersystem.service.user.UserReadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,9 @@ public class UserController {
     @Autowired
     private UserCreationService userCreationService;
 
+    @Autowired
+    private UserReadService userReadService;
+
     @PostMapping("/createUser")
     public User createUser(@RequestBody User user) {
 
@@ -23,5 +27,23 @@ public class UserController {
     public Collection<User> createUsers(@RequestBody Collection<User> users) {
 
         return userCreationService.createUsers(users);
+    }
+
+    @GetMapping("/users/{id}")
+    public User readUserById(@PathVariable Long id) {
+
+        return userReadService.readUserById(id);
+    }
+
+    @GetMapping("/users")
+    public User readUserByEmail(@RequestParam String email) {
+
+        return userReadService.readUserByEmail(email);
+    }
+
+    @GetMapping("/users")
+    public Collection<User> readUsers() {
+
+        return userReadService.readUsers();
     }
 }
