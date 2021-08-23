9ace2278a16061b8909e905be1a9d1b55046d1a7
David <d6797125@pm.me>
2021-08-23 22:41:13 +0200

    feat(service): update UserUpdateService

--- src/main/java/com/openclassrooms/moneytransfersystem/service/user/UserUpdateService.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/service/user/UserUpdateService.java
@@ -1,9 +1,40 @@
 package com.openclassrooms.moneytransfersystem.service.user;
 
+import com.openclassrooms.moneytransfersystem.dao.UserRepository;
+import com.openclassrooms.moneytransfersystem.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserUpdateService {
 
+    @Autowired
+    private UserRepository userRepository;
 
+    Logger logger = LoggerFactory.getLogger(UserUpdateService.class);
+
+    public User updateUser(User user) {
+
+        User userUpdated;
+        Optional<User> optionalUser = userRepository.findById(user.getId());
+
+        if (optionalUser.isPresent()) {
+            userUpdated = optionalUser.get();
+            //Todo: See if it gets the data properly... If not, set them.
+            //userUpdated.setEmail(optionalUser.get().getEmail());
+            userRepository.save(userUpdated);
+        } else {
+            logger.debug("Tried to update single " + User.class.getName()
+                    + " id: " + user.getId() + " (not found)");
+
+            return new User();
+        }
+        logger.debug("Update single " + User.class.getName() + ": " + userUpdated);
+
+        return userUpdated;
+    }
 }
