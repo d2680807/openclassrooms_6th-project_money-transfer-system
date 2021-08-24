0dfe2f5f552f5eb6d6409ab93f18d505b5f0e44c
David <d6797125@pm.me>
2021-08-24 18:13:19 +0200

    feat(security): update CustomUserDetails

--- src/main/java/com/openclassrooms/moneytransfersystem/configuration/CustomUserDetails.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/configuration/CustomUserDetails.java
@@ -32,4 +32,27 @@ public class CustomUserDetails  implements UserDetails {
 
         return user.getEmail();
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+
+        return true;
+    }
 }
