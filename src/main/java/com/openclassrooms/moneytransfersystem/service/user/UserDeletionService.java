58ea7f54395bf827d08995424ce53d5b529de64b
David <d6797125@pm.me>
2021-08-24 17:29:09 +0200

    feat(service): update UserDeletionService

--- src/main/java/com/openclassrooms/moneytransfersystem/service/user/UserDeletionService.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/service/user/UserDeletionService.java
@@ -1,9 +1,31 @@
 package com.openclassrooms.moneytransfersystem.service.user;
 
+import com.openclassrooms.moneytransfersystem.dao.UserRepository;
+import com.openclassrooms.moneytransfersystem.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDeletionService {
 
+    @Autowired
+    private UserRepository userRepository;
 
+    Logger logger = LoggerFactory.getLogger(UserDeletionService.class);
+
+    public void deleteUserById(Long id) {
+
+        if(userRepository.existsById(id)) {
+            logger.debug("Delete " + User.class.getName() + " by id: " + id);
+            userRepository.deleteById(id);
+        }
+    }
+
+    public void deleteUsers() {
+
+        logger.debug("Delete all " + User.class.getName());
+        userRepository.deleteAll();
+    }
 }
