63e84700ac34fd7f03d9353d918e88d43b591a80
David <d6797125@pm.me>
2021-08-24 18:35:54 +0200

    feat(security): update LoginController

--- src/main/java/com/openclassrooms/moneytransfersystem/controller/LoginController.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.openclassrooms.moneytransfersystem.controller;
 
+import com.openclassrooms.moneytransfersystem.dao.FriendshipRepository;
+import com.openclassrooms.moneytransfersystem.dao.TransferRepository;
+import com.openclassrooms.moneytransfersystem.dao.UserRepository;
 import com.openclassrooms.moneytransfersystem.model.*;
 import com.openclassrooms.moneytransfersystem.service.user.UserCreationService;
 import com.openclassrooms.moneytransfersystem.service.user.UserReadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.*;
 
@@ -25,6 +30,14 @@ public class LoginController {
     public String viewHomePage() {
 
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+
+        model.addAttribute("user", new User());
+
+        return "signup_form";
     }
 
     @GetMapping("/app")
