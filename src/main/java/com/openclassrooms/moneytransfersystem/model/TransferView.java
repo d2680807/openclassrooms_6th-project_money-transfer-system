009987fc67ab8916fa949420d03336d1bacb9fb8
David <d6797125@pm.me>
2021-08-24 18:22:29 +0200

    build(model): update TransferView

--- src/main/java/com/openclassrooms/moneytransfersystem/model/TransferView.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/model/TransferView.java
@@ -2,8 +2,13 @@ package com.openclassrooms.moneytransfersystem.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class TransferView {
 
-    
+    LocalDateTime date;
+    String relation;
+    String description;
+    String amount;
 }
