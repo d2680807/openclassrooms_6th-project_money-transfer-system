cdc92f6094d60ecf22d97b92d76c61adc5c5df01
David <d6797125@pm.me>
2021-08-24 18:12:29 +0200

    feat(security): update CustomUserDetails

--- src/main/java/com/openclassrooms/moneytransfersystem/configuration/CustomUserDetails.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/configuration/CustomUserDetails.java
@@ -1,6 +1,17 @@
 package com.openclassrooms.moneytransfersystem.configuration;
 
-public class CustomUserDetails {
+import com.openclassrooms.moneytransfersystem.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-    
+import java.util.Collection;
+
+public class CustomUserDetails  implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+
+        this.user = user;
+    }
 }
