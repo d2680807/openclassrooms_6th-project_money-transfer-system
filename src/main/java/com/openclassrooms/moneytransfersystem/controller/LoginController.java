45877f62eeb7f4a22c7b9ced674244fa77e09a61
David <d6797125@pm.me>
2021-08-24 18:25:46 +0200

    feat(security): update LoginController

--- src/main/java/com/openclassrooms/moneytransfersystem/controller/LoginController.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/controller/LoginController.java
@@ -6,11 +6,9 @@ import com.openclassrooms.moneytransfersystem.service.user.UserReadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.*;
 
@@ -27,5 +25,31 @@ public class LoginController {
     public String viewHomePage() {
 
         return "index";
+    }
+
+    @GetMapping("/app")
+    public String listTransfers(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userReadService.readUserByEmail(authentication.getName());
+
+        List<TransferView> listTransfers = new ArrayList<>();
+        user.getTransfers().stream()
+                .forEach( t -> {
+                    TransferView transfer = new TransferView();
+                    transfer.setDate(t.getDate());
+                    transfer.setRelation(t.getUser().getFirstName());
+                    transfer.setDescription(t.getDescription());
+                    if (t.getType() == TransferType.INGOING) {
+                        transfer.setAmount("+ " + String.valueOf(t.getAmount()));
+                    } else if (t.getType() == TransferType.OUTGOING) {
+                        transfer.setAmount("- " + String.valueOf(t.getAmount()));
+                    }
+                    listTransfers.add(transfer);
+                });
+
+        Collections.sort(listTransfers, Comparator.comparing(TransferView::getDate));
+        model.addAttribute("listTransfers", listTransfers);
+
+        return "app";
     }
 }
