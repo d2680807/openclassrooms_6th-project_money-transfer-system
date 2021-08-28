1c775dddd48a01bed8d9062f2935607dde842397
David <d6797125@pm.me>
2021-08-28 21:59:55 +0200

    build(model): update Tax model

--- src/main/java/com/openclassrooms/moneytransfersystem/model/Tax.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/model/Tax.java
@@ -2,13 +2,20 @@ package com.openclassrooms.moneytransfersystem.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "tax")
 public class Tax {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tax_id")
+    private Long id;
 
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private double rate;
 }
