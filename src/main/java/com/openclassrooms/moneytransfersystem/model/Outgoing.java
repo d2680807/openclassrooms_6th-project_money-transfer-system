edc52c0c52e0e3832d61d2dbed467dbd7c2a4995
David <d6797125@pm.me>
2021-08-30 10:04:22 +0200

    build(model): update Outgoing model

--- src/main/java/com/openclassrooms/moneytransfersystem/model/Outgoing.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/model/Outgoing.java
@@ -2,11 +2,33 @@ package com.openclassrooms.moneytransfersystem.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "outgoing")
 public class Outgoing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transfer_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+    @Column(nullable = false)
+    private double amount;
+    @Column(nullable = false)
+    private double tax;
+    @Column(nullable = true)
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "user",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private User user;
 }
