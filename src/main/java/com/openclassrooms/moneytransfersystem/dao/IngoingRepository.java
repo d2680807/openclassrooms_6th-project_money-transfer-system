18098288d2127cfda3f28c973499278d46c62722
David <d6797125@pm.me>
2021-08-30 10:07:07 +0200

    build(repo): rename TransferRepository to IngoingRepository

--- src/main/java/com/openclassrooms/moneytransfersystem/dao/IngoingRepository.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/dao/IngoingRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransferRepository extends JpaRepository<Ingoing, Long> {
+public interface IngoingRepository extends JpaRepository<Ingoing, Long> {
 
 
 }
