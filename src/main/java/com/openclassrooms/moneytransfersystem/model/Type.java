5e58e4a45db5a166ac0234312175f32258403830
David <d6797125@pm.me>
2021-08-30 14:23:30 +0200

    build(model): update Type model

--- src/main/java/com/openclassrooms/moneytransfersystem/model/Type.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/model/Type.java
@@ -2,13 +2,18 @@ package com.openclassrooms.moneytransfersystem.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "type")
 public class Type {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "type_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
 }
