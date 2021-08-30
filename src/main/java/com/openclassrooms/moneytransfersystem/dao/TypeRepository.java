d780f2d0f774db12f7698738f59add410767ba04
David <d6797125@pm.me>
2021-08-30 14:59:52 +0200

    build(repo): add TypeRepository

--- src/main/java/com/openclassrooms/moneytransfersystem/dao/TypeRepository.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/dao/TypeRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaxRepository extends JpaRepository<Transfer, Long> {
-
-
+public interface TypeRepository extends JpaRepository<Transfer, Long> {
 }
