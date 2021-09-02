e1d10ea3aa3b314ae66e27f35f43219ec23c1544
David <d6797125@pm.me>
2021-09-02 17:41:03 +0200

    feat(service): update TransferCreationService

--- src/main/java/com/openclassrooms/moneytransfersystem/service/transfer/TransferCreationService.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/service/transfer/TransferCreationService.java
@@ -1,9 +1,35 @@
 package com.openclassrooms.moneytransfersystem.service.transfer;
 
+import com.openclassrooms.moneytransfersystem.dao.TransferRepository;
+import com.openclassrooms.moneytransfersystem.model.Transfer;
+import com.openclassrooms.moneytransfersystem.model.User;
+import com.openclassrooms.moneytransfersystem.service.user.UserCreationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class TransferCreationService {
 
-    
+    @Autowired
+    private TransferRepository transferRepository;
+
+    Logger logger = LoggerFactory.getLogger(UserCreationService.class);
+
+    public Transfer createTransfer(Transfer transfer) {
+
+        logger.debug("[" + User.class.getName() + " - createTransfer]: " + transfer);
+
+        return transferRepository.save(transfer);
+    }
+
+    public Collection<Transfer> createTransfers(Collection<Transfer> transfers) {
+
+        logger.debug("[" + User.class.getName() + " - createTransfers]: " + transfers);
+
+        return transferRepository.saveAll(transfers);
+    }
 }
