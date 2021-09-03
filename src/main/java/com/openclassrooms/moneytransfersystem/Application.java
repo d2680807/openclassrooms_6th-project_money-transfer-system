3970e23d3705fc07b9a6c2c9f587120f656f6a58
David <d6797125@pm.me>
2021-09-03 10:18:11 +0200

    build(refac): rename MoneyTransferSystemApplication to Application

--- src/main/java/com/openclassrooms/moneytransfersystem/Application.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoneyTransferSystemApplication {
+public class Application {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(MoneyTransferSystemApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
