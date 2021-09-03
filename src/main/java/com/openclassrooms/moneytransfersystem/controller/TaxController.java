9501afe6ba60b84e85c24c9657b5b2b4e84588dd
David <d6797125@pm.me>
2021-09-03 13:55:44 +0200

    build(rest): add TaxController

---
+++ src/main/java/com/openclassrooms/moneytransfersystem/controller/TaxController.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.moneytransfersystem.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaxController {
+
+    
+}
