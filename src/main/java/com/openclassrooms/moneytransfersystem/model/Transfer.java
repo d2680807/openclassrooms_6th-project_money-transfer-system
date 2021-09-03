37f8ab606f634450a7b047c3fd3e02d576e8b5cc
David <d6797125@pm.me>
2021-09-03 19:47:06 +0200

    hotfix(model): fix Lombok @Data Exception

--- src/main/java/com/openclassrooms/moneytransfersystem/model/Transfer.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/model/Transfer.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
 @Entity
 @Table(name = "transfer")
 public class Transfer {
@@ -31,4 +30,60 @@ public class Transfer {
     @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name="user_id", updatable=false)
     private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TransferType getType() {
+        return type;
+    }
+
+    public void setType(TransferType type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
