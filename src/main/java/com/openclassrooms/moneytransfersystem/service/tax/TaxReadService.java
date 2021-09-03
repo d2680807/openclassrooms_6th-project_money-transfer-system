ab6a1ecb028a2faf9612ef3256adff0924812e95
David <d6797125@pm.me>
2021-09-03 09:48:50 +0200

    feat(service): update TaxReadService

--- src/main/java/com/openclassrooms/moneytransfersystem/service/tax/TaxReadService.java
+++ src/main/java/com/openclassrooms/moneytransfersystem/service/tax/TaxReadService.java
@@ -1,9 +1,33 @@
 package com.openclassrooms.moneytransfersystem.service.tax;
 
+import com.openclassrooms.moneytransfersystem.dao.TaxRepository;
+import com.openclassrooms.moneytransfersystem.model.Tax;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class TaxReadService {
 
-    
+    @Autowired
+    private TaxRepository taxRepository;
+
+    Logger logger = LoggerFactory.getLogger(TaxReadService.class);
+
+    public Tax readTaxById(Long id) {
+
+        logger.debug("[readTaxById] id:" + id);
+
+        return taxRepository.getById(id);
+    }
+
+    public Collection<Tax> readTaxes() {
+
+        logger.debug("[readTaxes] find: all");
+
+        return taxRepository.findAll();
+    }
 }
