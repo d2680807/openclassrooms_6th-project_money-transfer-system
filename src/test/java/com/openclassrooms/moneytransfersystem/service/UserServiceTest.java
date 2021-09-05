ef511013aa405359b813d4c0f8d3455b1f534bfd
David <d6797125@pm.me>
2021-09-05 11:35:22 +0200

    test(service): add UserServiceTest

---
+++ src/test/java/com/openclassrooms/moneytransfersystem/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.moneytransfersystem.service;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserServiceTest {
+
+    
+}
