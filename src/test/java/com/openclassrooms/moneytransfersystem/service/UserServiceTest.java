c7e6f4a872a2f1e4385932b70a5860cbcc3835f5
David <d6797125@pm.me>
2021-09-05 19:08:54 +0200

    test(service): update TaxServiceTest

---
+++ src/test/java/com/openclassrooms/moneytransfersystem/service/UserServiceTest.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.moneytransfersystem.service;
+
+import com.openclassrooms.moneytransfersystem.controller.UserController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(UserController.class)
+public class UserServiceTest {
+
+    
+}
