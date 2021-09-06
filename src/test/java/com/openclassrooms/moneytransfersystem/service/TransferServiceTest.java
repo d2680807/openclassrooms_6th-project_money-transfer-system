d9cf8a6d27a47bbbaecc7b8bc7261513cedec10b
David <d6797125@pm.me>
2021-09-06 17:21:20 +0200

    test(service): add TaxServiceTest

--- src/test/java/com/openclassrooms/moneytransfersystem/service/TransferServiceTest.java
+++ src/test/java/com/openclassrooms/moneytransfersystem/service/TransferServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -44,76 +45,6 @@ public class TransferServiceTest {
     private TransferUpdateService transferUpdateService;
     @MockBean
     private TransferDeletionService transferDeletionService;
-
-    @Test
-    public void shouldCreateTransfer() throws Exception {
-
-        User user = new User();
-        user.setId(57L);
-
-        Transfer transfer = new Transfer();
-        transfer.setId(1L);
-        transfer.setUser(user);
-        transfer.setDate(LocalDateTime.now());
-        transfer.setType(TransferType.OUT);
-        transfer.setAmount(50);
-        transfer.setTax(0.05);
-        transfer.setDescription("Remboursement pour le cine.");
-
-        Transfer transferSaved = new Transfer();
-        transferSaved.setId(1L);
-        transferSaved.setUser(user);
-        transferSaved.setDate(LocalDateTime.now());
-        transferSaved.setType(TransferType.OUT);
-        transferSaved.setAmount(50);
-        transferSaved.setTax(0.05);
-        transferSaved.setDescription("Remboursement pour le cine.");
-
-        Mockito.when(transferCreationService.createTransfer(transfer)).thenReturn(transferSaved);
-
-        mockMvc.perform(post("/createTransfer")
-                .contentType("application/json")
-                .content(objectMapper.writeValueAsString(transfer))
-        ).andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(transferSaved))).andReturn();
-    }
-
-    @Test
-    public void shouldCreateTransfers() throws Exception {
-
-        User user = new User();
-        user.setId(57L);
-
-        Transfer transfer = new Transfer();
-        transfer.setUser(user);
-        transfer.setDate(LocalDateTime.now());
-        transfer.setType(TransferType.OUT);
-        transfer.setAmount(50);
-        transfer.setTax(0.05);
-        transfer.setDescription("Remboursement pour le cine.");
-
-        List<Transfer> transfers = new ArrayList<>();
-        transfers.add(transfer);
-
-        Transfer transferSaved = new Transfer();
-        transferSaved.setUser(user);
-        transferSaved.setDate(LocalDateTime.now());
-        transferSaved.setType(TransferType.OUT);
-        transferSaved.setAmount(50);
-        transferSaved.setTax(0.05);
-        transferSaved.setDescription("Remboursement pour le cine.");
-
-        List<Transfer> transfersSaved = new ArrayList<>();
-        transfersSaved.add(transferSaved);
-
-        Mockito.when(transferCreationService.createTransfers(transfers)).thenReturn(transfersSaved);
-
-        mockMvc.perform(post("/createTransfers")
-                .contentType("application/json")
-                .content(objectMapper.writeValueAsString(transfers))
-        ).andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(transfersSaved))).andReturn();
-    }
 
     @Test
     public void shouldGetTransfers() throws Exception {
@@ -165,39 +96,6 @@ public class TransferServiceTest {
         String expectedResponse = objectMapper.writeValueAsString(transfer);
 
         assertEquals(actualResponse, expectedResponse);
-    }
-
-    @Test
-    public void shouldUpdateTransfer() throws Exception {
-
-        User user = new User();
-        user.setId(57L);
-
-        Transfer transfer = new Transfer();
-        transfer.setId(53L);
-        transfer.setUser(user);
-        transfer.setDate(LocalDateTime.now());
-        transfer.setType(TransferType.OUT);
-        transfer.setAmount(50);
-        transfer.setTax(0.05);
-        transfer.setDescription("Remboursement pour le cine.");
-
-        Transfer transferUpdated = new Transfer();
-        transferUpdated.setId(53L);
-        transferUpdated.setUser(user);
-        transferUpdated.setDate(LocalDateTime.now());
-        transferUpdated.setType(TransferType.OUT);
-        transferUpdated.setAmount(50);
-        transferUpdated.setTax(0.05);
-        transferUpdated.setDescription("Remboursement pour le cine.");
-
-        Mockito.when(transferUpdateService.updateTransfer(transfer)).thenReturn(transferUpdated);
-
-        mockMvc.perform(put("/updateTransfer")
-                .contentType("application/json")
-                .content(objectMapper.writeValueAsString(transfer)))
-                .andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(transferUpdated)));
     }
 
     @Test
