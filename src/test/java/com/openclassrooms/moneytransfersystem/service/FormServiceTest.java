6011accd0f4522b4f5d7f7bcfa68090cba38f56e
David <d6797125@pm.me>
2021-09-07 17:19:33 +0200

    test(service): update FormServiceTest

--- src/test/java/com/openclassrooms/moneytransfersystem/service/FormServiceTest.java
+++ src/test/java/com/openclassrooms/moneytransfersystem/service/FormServiceTest.java
@@ -1,9 +1,15 @@
 package com.openclassrooms.moneytransfersystem.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.openclassrooms.moneytransfersystem.model.utility.ListElement;
+import com.openclassrooms.moneytransfersystem.model.utility.Requirement;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,5 +27,60 @@ public class FormServiceTest {
                 .getBalance("test@test.com");
 
         assertEquals(expectedBalance, actualBalance);
+    }
+
+    @Test
+    void shouldGetBalanceBack() throws ParseException {
+
+        NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+        Number balance = format.parse(formService
+                .getBalance("test@test.com"));
+        String expectedBalance = String.format("%.2f",
+                balance.doubleValue() - 10) ;
+
+        Requirement requirement = new Requirement();
+        requirement.setUserId(57L);
+        requirement.setAmount(10);
+        formService
+                .updateBalance(requirement, false);
+
+        String actualBalance = formService
+                .getBalance("test@test.com");
+
+        assertEquals(expectedBalance, actualBalance);
+    }
+
+    @Test
+    void shouldGetRequirement() {
+
+        Requirement expectedRequirement = new Requirement();
+        expectedRequirement.setUserId(57L);
+
+        Requirement actualRequirement = formService
+                .getRequirement("test@test.com");
+
+        assertEquals(expectedRequirement, actualRequirement);
+    }
+
+    @Test
+    void shouldGetFriendsList() throws JsonProcessingException {
+
+        Set<String> expectedFriendsList = new HashSet<>();
+
+        Set<String> actualFriendsList = formService
+                .getFriendsList("test@test.com");
+
+        assertEquals(expectedFriendsList, actualFriendsList);
+    }
+
+    @Test
+    void shouldGetTransfersList() throws JsonProcessingException {
+
+        List<ListElement> expectedTransfersList = new ArrayList<>();
+
+        List<ListElement> actualTransfersList = formService
+                .getTransfersList("test@test.com");
+
+        assertEquals(expectedTransfersList, actualTransfersList);
     }
 }
