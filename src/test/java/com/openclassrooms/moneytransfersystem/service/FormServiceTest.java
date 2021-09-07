a028dcc522130ad1b9e1aeb1c4b72fa4cc90c8cc
David <d6797125@pm.me>
2021-09-07 16:56:43 +0200

    test(service): update FormServiceTest

--- src/test/java/com/openclassrooms/moneytransfersystem/service/FormServiceTest.java
+++ src/test/java/com/openclassrooms/moneytransfersystem/service/FormServiceTest.java
@@ -1,11 +1,25 @@
 package com.openclassrooms.moneytransfersystem.service;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-@AutoConfigureMockMvc
 public class FormServiceTest {
 
+    @Autowired
+    private FormService formService;
 
+    @Test
+    void shouldGetBalance() {
+
+        String expectedBalance = "0,00";
+        String actualBalance = formService
+                .getBalance("test@test.com");
+
+        assertEquals(expectedBalance, actualBalance);
+    }
 }
